03c09740ea0e795afb4e67878068b53ef58add9f
He0rhi <egooorrrnok@gmail.com>
2024-06-25 20:34:03 +0300

    All curse

---
+++ JS_Core_Advanced_course/async_await/async_await.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+async function fetchApi(urls) {
+
+
+    const requests = urls.map(url => axios.get(url));
+
+    const responses = await Promise.all(requests);
+
+
+    const data = responses.map(response => response.data);
+
+    const allData = data.reduce((sum, data) => {
+      return sum.concat(data);
+    }, []);
+
+    return allData;
+  
+}
+const urls = ['http://bit.ly/2mTM3nY','http://bit.ly/2mTM3nY','http://bit.ly/2mTM3nY']
+
+
+fetchApi(urls)
+  .then(allData => {
+    console.log(allData);
+  })
--- JS_Core_Advanced_course/promise/promise.js
+++ JS_Core_Advanced_course/promise/promise.js
@@ -1,26 +1,19 @@
 const axios = require('axios');
 
-async function fetchMultipleAPIs(urls) {
 
+function fetchApi(urls) {
+  const promises = urls.map(url => axios.get(url));
 
-    const requests = urls.map(url => axios.get(url));
-
-    const responses = await Promise.all(requests);
-
-
-    const data = responses.map(response => response.data);
-
-    const allData = data.reduce((sum, data) => {
-      return sum.concat(data);
-    }, []);
-
-    return allData;
-  
+  return Promise.all(promises)
+    .then(responses => {
+      return responses.map(response => response.data);
+    })
+   
 }
 const urls = ['http://bit.ly/2mTM3nY','http://bit.ly/2mTM3nY','http://bit.ly/2mTM3nY']
 
 
-fetchMultipleAPIs(urls)
+fetchApi(urls)
   .then(allData => {
     console.log(allData);
   })
