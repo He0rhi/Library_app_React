6d4ca0a9779d9c1e4d3614d18969a3ce8bbae71b
He0rhi <egooorrrnok@gmail.com>
2024-07-03 12:49:17 +0300

    refactor: В модуле SearchForm был использован маппинг для отрисовки селектов

--- library_app/src/components/SearchForm.tsx
+++ library_app/src/components/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Form, Button, FormControl, InputGroup, Row, Col, FormGroup } from 'react-bootstrap';
 
 interface SearchFormProps {
@@ -11,86 +11,90 @@ interface SearchFormProps {
   handleSearch: () => void;
 }
 
-const SearchForm: React.FC<SearchFormProps> = React.memo(({ searchName, setSearchName, category, setCategory, sorting, setSorting, handleSearch }) => {
-  
-  const memoizedHandleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    handleSearch();
-  }, [handleSearch]);
-
-  const memoizedHandleButtonClick = useCallback(() => {
-    handleSearch();
-  }, [handleSearch]);
-
-  const memoizedSetSearchName = useCallback((e:any) => {
-    setSearchName(e.target.value);
-  }, [setSearchName]);
-
-  const memoizedSetCategory = useCallback((e:any) => {
-    setCategory(e.target.value);
-  }, [setCategory]);
-
-  const memoizedSetSorting = useCallback((e:any) => {
-    setSorting(e.target.value);
-  }, [setSorting]);
+const SearchForm: React.FC<SearchFormProps> = ({
+  searchName,
+  setSearchName,
+  category,
+  setCategory,
+  sorting,
+  setSorting,
+  handleSearch
+}) => {
+  const categoryOptions = [
+    { value: '', label: 'All' },
+    { value: 'art', label: 'Art' },
+    { value: 'computers', label: 'Computers' },
+    { value: 'history', label: 'History' },
+    { value: 'medical', label: 'Medical' },
+    { value: 'poetry', label: 'Poetry' }
+  ];
 
   return (
     <div className='search_control search_control_tint'>
       <div className='header_main'>
         <div className='header_logo'>Search for books</div>
       </div>
-      <Form onSubmit={memoizedHandleSubmit}>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch();
+        }}
+      >
         <InputGroup>
           <div className='input_group_form_button'>
-            <Form.Control 
-              id="searchName" 
-              name="searchName"
-              className='form_control' 
+            <Form.Control
+              id='searchName'
+              name='searchName'
+              className='form_control'
               placeholder='Search...'
               value={searchName}
-              onChange={memoizedSetSearchName}
-              onKeyDown={(e:any) => {
+              onChange={(e) => setSearchName(e.target.value)}
+              onKeyDown={(e) => {
                 if (e.key === 'Enter') {
-                  memoizedHandleSubmit(e);
+                  handleSearch();
                 }
               }}
             />
-            <Button className='search_button' onClick={memoizedHandleButtonClick}></Button>
+            <Button className='search_button' onClick={handleSearch}></Button>
           </div>
         </InputGroup>
         <Row className='rows'>
           <Col className='cols'>
             <FormGroup className='category_control'>
-              <Form.Label className='control_label' htmlFor='category'>Category:</Form.Label>
-              <FormControl 
-                className='category_control_select' 
-                as="select" 
-                id="category"
-                name="category"
+              <Form.Label className='control_label' htmlFor='category'>
+                Category:
+              </Form.Label>
+              <FormControl
+                className='category_control_select'
+                as='select'
+                id='category'
+                name='category'
                 value={category}
-                onChange={memoizedSetCategory}
+                onChange={(e) => setCategory(e.target.value)}
               >
-                <option value="">All</option>
-                <option value="art">Art</option>
-                <option value="computers">Computers</option>
-                <option value="history">History</option>
-                <option value="medical">Medical</option>
-                <option value="poetry">Poetry</option>
+                {categoryOptions.map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
               </FormControl>
             </FormGroup>
           </Col>
           <Col>
             <FormGroup className='sorting_control'>
-              <Form.Label className='control_label' htmlFor='sorting'>Sorting:</Form.Label>
-              <FormControl 
-                className='category_control_select' 
-                as="select" 
-                id="sorting"
-                name="sorting"
+              <Form.Label className='control_label' htmlFor='sorting'>
+                Sorting:
+              </Form.Label>
+              <FormControl
+                className='category_control_select'
+                as='select'
+                id='sorting'
+                name='sorting'
                 value={sorting}
+                onChange={(e) => setSorting(e.target.value)}
               >
-                <option value="relevance">Relevance</option>
-                <option value="newest">Newest</option>
+                <option value='relevance'>Relevance</option>
+                <option value='newest'>Newest</option>
               </FormControl>
             </FormGroup>
           </Col>
@@ -98,6 +102,6 @@ const SearchForm: React.FC<SearchFormProps> = React.memo(({ searchName, setSearc
       </Form>
     </div>
   );
-});
+};
 
 export default SearchForm;
