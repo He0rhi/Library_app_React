4d07409e585a4c96e47c8572fb972a7365046e4f
He0rhi <95845898+He0rhi@users.noreply.github.com>
2025-03-21 18:58:08 +0300

    Delete JsPracticeReact directory

--- JsPracticeReact/PositiveNegative/positivenegative/src/Message.js
+++
@@ -1,8 +0,0 @@
-function Message({num}){
-return(
-    <div>
-        {num>0?(<p>`Число {num} положительное`</p>):num<0?(<p>Число {num} отрицательное</p>):(<p>Введите</p>)}
-    </div>
-);
-};
-export default Message;
