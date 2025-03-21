e29b68804b1e21f6f89cb598b06fc36761ae1bb4
He0rhi <95845898+He0rhi@users.noreply.github.com>
2025-03-21 18:57:58 +0300

    Delete JS_Core_Advanced_course directory

--- JS_Core_Advanced_course/arrays/arrays.js
+++
@@ -1,6 +0,0 @@
-//Напишите функцию, которая принимает массив с числами и находит сумму квадратов элементов этого массива.
-function SumSquare(arr){
-return arr.reduce((sum,current)=>
-     sum + current*current
-)}
-console.log( SumSquare([1,2,3,4,5]));
--- JS_Core_Advanced_course/use_strict/usestrict.js
+++
@@ -1,9 +0,0 @@
-//Напишите функцию, которая принимает строку и возвращает количество гласных букв в ней. Используйте строгий режим
-"use strict";
-
-function vowelsCount(arr){
-    const vowels = /[aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ]/g;
-    const countvowels = arr.match(vowels);
-    console.log("Гласных букв: " +countvowels.length );
-}
-vowelsCount("Гласные буквы");
